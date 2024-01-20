c2977a1b98c0af0d520968bb7aa09548c3b084b3
Neil Sagar Sahu <neilsagar55555@gmail.com>
2024-01-20 12:20:22 +0530

    all files

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
+
+function App() {
+  const storedNotes = JSON.parse(localStorage.getItem("keeper-app-notes")) || [];
+  const [notes, setNotes] = useState(storedNotes);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  useEffect(() => {
+    localStorage.setItem("keeper-app-notes", JSON.stringify(notes));
+  }, [notes]);
+  
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes && notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
